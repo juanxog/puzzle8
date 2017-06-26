6f6f82dcede7ab6a755eb03b4bbcb14e994de62f
Juan Girón <Juan Girn>
2017-06-26 14:21:59 -0500

    fixed bfs-dfs

--- driver.py
+++ driver.py
@@ -1,13 +1,13 @@
 import copy
-import sys
-sys.setrecursionlimit(100000)
 
 class node:
 
-    def __init__(self,state,parent,action):
+    def __init__(self,id,state,parent,action,level):
+        self.id = id
         self.state = state
         self.parent = parent
         self.action = action
+        self.level = level
 
 def move_up(universe, pos, action):
 
@@ -54,24 +54,24 @@ def find_moves(universe,pos):
     posibilities = []
 
     if move_up(universe,pos,False):
-        posibilities.append("UP")
+        posibilities.append("Up")
     if move_down(universe,pos,False):
-        posibilities.append("DOWN")
+        posibilities.append("Down")
     if move_left(universe, pos,False):
-        posibilities.append("LEFT")
+        posibilities.append("Left")
     if move_right(universe, pos,False):
-        posibilities.append("RIGHT")
+        posibilities.append("Right")
 
     return posibilities
 
 def action_moves(universe,pos,action):
-    if action == 'UP':
+    if action == 'Up':
         move_up(universe,pos,True)
-    if action == 'DOWN':
+    if action == 'Down':
         move_down(universe, pos, True)
-    if action == 'LEFT':
+    if action == 'Left':
         move_left(universe, pos, True)
-    if action == 'RIGHT':
+    if action == 'Right':
         move_right(universe, pos, True)
 
 
@@ -93,15 +93,15 @@ def find_index_node(nodes,node):
         if nodes[i].state == node:
             return i
 
+
 def find_path(nodes,node,path):
     index = find_index_node(nodes, node)
     while True:
-        if nodes[index].action <> "":
+        if nodes[index].action <> "" and index <> -1:
             path.append(nodes[index].action)
-            index = find_index_node(nodes, nodes[index].parent)
+            index = nodes[index].parent
         else:
             break
-
 
 def reverse(actions):
     new_action = []
@@ -110,159 +110,136 @@ def reverse(actions):
     return  new_action
 
 
-def bfs(universe):
 
+
+def dfs(universe):
     pos_zero = -1
     visited = []
     frontier = []
     nodes = []
-    frontier.append(universe)
-    nodes.append(node(universe,[],""))
+    nodes.append(node(0, universe, -1, "", 0))
+    frontier.append(nodes[len(nodes) - 1])
     foundgoal = False
     nodes_expanded = 0
     posibles_level = []
 
-
     while len(frontier) <> 0 and not foundgoal:
 
         state = frontier.pop(0)
-        parent = copy.copy(state)
-        visited.append(copy.copy(state))
+        parent = state.id
+        visited.append(state.state)
 
-        if isgoal(state):
+        if isgoal(state.state):
             foundgoal = True
 
-        for i in state:
-            if state[i] == 0:
+        for i in state.state:
+            if state.state[i] == 0:
                 pos_zero = i
 
         if not foundgoal:
-            print nodes_expanded
             nodes_expanded = nodes_expanded + 1
-            posibles_moves = find_moves(state, pos_zero)
-            posibles = 0
+            posibles_moves = find_moves(state.state, pos_zero)
 
-            for x in posibles_moves:
-                aux_universe = copy.copy(state)
-                action_moves(aux_universe,pos_zero,x)
-                if(aux_universe not in visited and aux_universe not in frontier):
-                    frontier.append(aux_universe)
-                    nodes.append(node(aux_universe, parent, x))
-                    posibles = posibles +1
+            for x in reversed(posibles_moves):
+                aux_universe = copy.copy(state.state)
+                action_moves(aux_universe, pos_zero, x)
 
-            posibles_level.append(posibles)
+                if (aux_universe not in visited):
+                    flag = False
+                    for k in frontier:
+                        if aux_universe == k.state:
+                            flag = False
+                            break
+                        else:
+                            flag = True
+
+                    if len(frontier) == 0:
+                        flag = True
+
+                    if flag:
+                        nodes.append(node(len(nodes), aux_universe, parent, x, nodes[parent].level + 1))
+                        frontier.insert(0, nodes[len(nodes) - 1])
 
     actions = []
-    find_path(nodes,[0,1,2,3,4,5,6,7,8],actions)
+    find_path(nodes, [0, 1, 2, 3, 4, 5, 6, 7, 8], actions)
 
-    ant = posibles_level[0]
-    entro = True
     index = 1
-    level = 1
+    level = nodes[0].level
 
-    while entro == True :
-
-        acum = 0
-
-        for y in range(index,index+ant):
-            if len(posibles_level) <= y:
-                break
-            acum = acum + posibles_level[y]
-            index = index + 1
-
-        ant = acum
-
-        if acum > 0:
-            level = level + 1
-
-        if len(posibles_level) <= index:
-            break
-
+    for x in nodes:
+        if x.level > level:
+            level = x.level
 
     print "path_to_goal: ", reverse(actions)
-    print "cost_of_path: "  , len(actions)
-    print "nodes_expanded: " , nodes_expanded
+    print "cost_of_path: ", len(actions)
+    print "nodes_expanded: ", nodes_expanded
     print "search_depth: ", len(actions)
     print "max_search_depth: ", level
     print "running_time:", 1.050121
     print "max_ram_usage:", 0.050121
 
 
-def dfs(universe):
-
+def bfs_new(universe):
     pos_zero = -1
     visited = []
     frontier = []
     nodes = []
-    frontier.append(universe)
-    nodes.append(node(universe,[],""))
+    nodes.append(node(0, universe, -1, "", 0))
+    frontier.append(nodes[len(nodes) - 1])
     foundgoal = False
     nodes_expanded = 0
     posibles_level = []
 
-
     while len(frontier) <> 0 and not foundgoal:
 
         state = frontier.pop(0)
-        parent = copy.copy(state)
-        visited.append(copy.copy(state))
+        parent = state.id
+        visited.append(state.state)
 
-        if isgoal(state):
+        if isgoal(state.state):
             foundgoal = True
 
-        for i in state:
-            if state[i] == 0:
+        for i in state.state:
+            if state.state[i] == 0:
                 pos_zero = i
 
         if not foundgoal:
-            print nodes_expanded
             nodes_expanded = nodes_expanded + 1
-            posibles_moves = find_moves(state, pos_zero)
-            posibles = 0
+            posibles_moves = find_moves(state.state, pos_zero)
 
-            for x in reversed(posibles_moves):
-                aux_universe = copy.copy(state)
-                action_moves(aux_universe,pos_zero,x)
-                if(aux_universe not in visited and aux_universe not in frontier):
-                    frontier.insert(0,aux_universe)
-                    nodes.append(node(aux_universe, parent, x))
-                    posibles = posibles +1
+            for x in posibles_moves:
+                aux_universe = copy.copy(state.state)
+                action_moves(aux_universe, pos_zero, x)
 
-            posibles_level.append(posibles)
+                if (aux_universe not in visited):
+                    flag = False
+                    for k in frontier:
+                        if aux_universe == k.state:
+                            flag = False
+                            break
+                        else:
+                            flag = True
 
-            #print "state", state
-            #print "frontier", frontier
+                    if len(frontier) == 0:
+                        flag = True
 
+                    if flag:
+                        nodes.append(node(len(nodes), aux_universe, parent, x, nodes[parent].level + 1))
+                        frontier.append(nodes[len(nodes) - 1])
 
     actions = []
-    find_path(nodes,[0,1,2,3,4,5,6,7,8],actions)
+    find_path(nodes, [0, 1, 2, 3, 4, 5, 6, 7, 8], actions)
 
-    ant = posibles_level[0]
-    entro = True
     index = 1
-    level = 1
+    level = nodes[0].level
 
-    while entro == True :
-
-        acum = 0
-
-        for y in range(index,index+ant):
-            if len(posibles_level) <= y:
-                break
-            acum = acum + posibles_level[y]
-            index = index + 1
-
-        ant = acum
-
-        if acum > 0:
-            level = level + 1
-
-        if len(posibles_level) <= index:
-            break
+    for x in nodes:
+        if x.level > level:
+            level = x.level
 
     print "path_to_goal: ", reverse(actions)
-    print "cost_of_path: "  , len(actions)
-    print "nodes_expanded: " , nodes_expanded
+    print "cost_of_path: ", len(actions)
+    print "nodes_expanded: ", nodes_expanded
     print "search_depth: ", len(actions)
     print "max_search_depth: ", level
     print "running_time:", 1.050121
@@ -271,7 +248,7 @@ def dfs(universe):
 def main(method,universe):
 
     if method == "bfs":
-        bfs(universe)
+        bfs_new(universe)
 
     if method == "dfs":
         dfs(universe)
@@ -279,4 +256,4 @@ def main(method,universe):
     if method == "ast":
         bfs(universe)
 
-main("dfs",[8,6,4,2,1,3,5,7,0])
+main("dfs",[1,2,5,3,4,0,6,7,8])
